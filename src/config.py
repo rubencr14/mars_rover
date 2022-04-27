231b4db02c738b2d8b4c596a0fbf4a527e4cdd92
ruben cañadas rodriguez <rubencr@MacBook-Pro-de-ruben.local>
2022-04-27 23:00:42 +0200

    config.py updated

--- src/config.py
+++ src/config.py
@@ -6,4 +6,3 @@ the Mars Rovers behavior and the size of the grid it will move.
 NUMBER_OF_OBSTACLES = 5
 GRID_SIZE = 10 #SQUARED GRID (N,N)
 DRAW_PATH = True
-PASS_TEST_FIRST = False #pass tests before execution
